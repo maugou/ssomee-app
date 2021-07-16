5a4aca918db49c9ed7858ec1f348601cfe3b9b6e
Jo Jae Hoon <dev.hoony@gmail.com>
2021-07-16 21:28:07 +0900

    상세 페이지 타이틀 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Text, StyleSheet } from 'react-native';
 
 import { ProductList } from './components/ProductList';
 import { ProductDetail } from './components/ProductDetail';
@@ -16,10 +17,25 @@ const App = () => {
           component={ProductList}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="ProductDetail" component={ProductDetail} />
+        <Stack.Screen
+          name="ProductDetail"
+          component={ProductDetail}
+          options={{
+            headerTitle: () => <Text style={styles.header}>SSOMEE</Text>,
+            headerBackTitleVisible: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'rgb(102, 051, 255)',
+  },
+});

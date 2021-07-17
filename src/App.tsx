e5ccf20285c129f1ecc71238a448f349552a4378
Jo Jae Hoon <dev.hoony@gmail.com>
2021-07-17 19:52:20 +0900

    장바구니 하단 탭 생성 및 기본 이동 구현
    - icons xcode 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { Text, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import { store } from './redux/store';
 import { ProductList } from './components/ProductList';
 import { ProductDetail } from './components/ProductDetail';
+import { Cart } from './components/Cart';
 
 const Stack = createStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const BottomTabRoute = () => {
+  return (
+    <BottomTab.Navigator
+      tabBarOptions={{ activeTintColor: 'rgb(255, 102, 000)' }}>
+      <BottomTab.Screen
+        name="목록"
+        component={ProductList}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="reorder-four-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="장바구니"
+        component={Cart}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="cart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+};
 
 const App = () => {
   return (
@@ -17,7 +47,7 @@ const App = () => {
         <Stack.Navigator>
           <Stack.Screen
             name="ProductList"
-            component={ProductList}
+            component={BottomTabRoute}
             options={{ headerShown: false }}
           />
           <Stack.Screen
@@ -26,6 +56,14 @@ const App = () => {
             options={{
               headerTitle: () => <Text style={styles.header}>SSOMEE</Text>,
               headerBackTitleVisible: false,
+              headerBackImage: () => (
+                <Icon
+                  name="arrow-back-outline"
+                  size={26}
+                  color="rgb(70, 70, 70)"
+                  style={styles.backArrow}
+                />
+              ),
             }}
           />
         </Stack.Navigator>
@@ -41,5 +79,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: 'rgb(102, 051, 255)',
+  },
+  backArrow: {
+    paddingHorizontal: 14,
   },
 });

1415c46672df81022db8e5a5507df225c1414e3f
Jo Jae Hoon <dev.hoony@gmail.com>
2021-07-18 17:38:17 +0900

    장바구니 관련 금액 표시

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import {
   SafeAreaView,
   FlatList,
@@ -16,13 +16,38 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { getPriceWithComma } from '../common/constant';
 import { RootState } from '../redux/store';
 import { removeFromCart } from '../redux/slice';
+import { isEmpty } from 'lodash';
 
 export const Cart = () => {
+  const [price, setPrice] = useState({ originalPrice: 0, ssomeePrice: 0 });
+
   const navigation = useNavigation();
   const dispatch = useDispatch();
 
   const cart = useSelector((store: RootState) => store.cart);
   const products = useSelector((store: RootState) => store.products);
+
+  useEffect(() => {
+    const cartPrice = cart.reduce(
+      (calculateRes, id) => {
+        if (isEmpty(calculateRes)) {
+          calculateRes.originalPrice = products[id].originalPrice;
+          calculateRes.ssomeePrice = products[id].ssomeePrice;
+        } else {
+          calculateRes.originalPrice += products[id].originalPrice;
+          calculateRes.ssomeePrice += products[id].ssomeePrice;
+        }
+
+        return calculateRes;
+      },
+      {
+        originalPrice: 0,
+        ssomeePrice: 0,
+      }
+    );
+
+    setPrice(cartPrice);
+  }, [products, cart]);
 
   const renderItem = useCallback(
     ({ item, index }) => {
@@ -68,6 +93,8 @@ export const Cart = () => {
   const ItemSeparatorComponent = () => {
     return <View style={styles.divideLine} />;
   };
+
+  console.log(price);
   return (
     <SafeAreaView style={styles.container}>
       {cart.length === 0 ? (
@@ -77,13 +104,33 @@ export const Cart = () => {
           </Text>
         </View>
       ) : (
-        <FlatList
-          data={cart}
-          renderItem={renderItem}
-          keyExtractor={keyEctractor}
-          showsVerticalScrollIndicator={false}
-          ItemSeparatorComponent={ItemSeparatorComponent}
-        />
+        <>
+          <FlatList
+            data={cart}
+            renderItem={renderItem}
+            keyExtractor={keyEctractor}
+            showsVerticalScrollIndicator={false}
+            ItemSeparatorComponent={ItemSeparatorComponent}
+          />
+
+          <View style={styles.divideLine} />
+
+          <View style={styles.checkPriceBox}>
+            <Text>할인금액</Text>
+            <Text>
+              {getPriceWithComma(price.originalPrice - price.ssomeePrice)} 원
+            </Text>
+          </View>
+          <View style={styles.checkPriceBox}>
+            <Text style={styles.orderPriceText}>총 구매금액</Text>
+            <Text style={styles.orderPriceText}>
+              {getPriceWithComma(price.ssomeePrice)} 원
+            </Text>
+          </View>
+          <TouchableOpacity style={styles.orderButton}>
+            <Text style={styles.orderText}>구매하기</Text>
+          </TouchableOpacity>
+        </>
       )}
     </SafeAreaView>
   );
@@ -159,5 +206,25 @@ const styles = StyleSheet.create({
     right: 4,
     padding: 6,
     paddingTop: 2,
+  },
+  checkPriceBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 16,
+    marginVertical: 12,
+  },
+  orderButton: {
+    backgroundColor: 'rgb(102, 051, 255)',
+    padding: 14,
+    margin: 16,
+  },
+  orderPriceText: {
+    fontWeight: 'bold',
+  },
+  orderText: {
+    textAlign: 'center',
+    color: 'rgb(255, 255, 255)',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
